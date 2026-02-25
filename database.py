be766925c951c24ff23361ea40fe470d0f45e71f
federicoandronico11 <federicoandronico11@gmail.com>
2026-02-26 00:36:10 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,11 +2,10 @@ import streamlit as st
 from datetime import datetime
 
 def init_session():
-    # Database Storici
+    # Database Storici (Permanenti nella sessione)
     if 'db_atleti' not in st.session_state: st.session_state['db_atleti'] = []
     if 'ranking_atleti' not in st.session_state: st.session_state['ranking_atleti'] = {}
     if 'albo_oro' not in st.session_state: st.session_state['albo_oro'] = []
-    if 'storico_incassi' not in st.session_state: st.session_state['storico_incassi'] = []
     if 'atleti_stats' not in st.session_state: st.session_state['atleti_stats'] = {}
     
     # Stato Torneo Corrente
@@ -16,7 +15,7 @@ def init_session():
     if 'phase' not in st.session_state: st.session_state['phase'] = "Setup"
     if 'match_type' not in st.session_state: st.session_state['match_type'] = "Best of 3"
     
-    # Nuove Impostazioni (Aggiunte)
+    # Impostazioni Tecniche
     if 'settings' not in st.session_state:
         st.session_state['settings'] = {
             "punti_set": 21,
@@ -26,17 +25,14 @@ def init_session():
         }
 
 def aggiorna_database_storico(nome_atleta, pf, ps, sv, sp, vittorie, piazzamento):
-    if nome_atleta == "N/A" or nome_atleta == "BYE": return
+    if nome_atleta in ["N/A", "BYE", "-"]: return
     if nome_atleta not in st.session_state['atleti_stats']:
         st.session_state['atleti_stats'][nome_atleta] = {
             "pf": 0, "ps": 0, "sv": 0, "sp": 0, 
             "partite_vinte": 0, "tornei_giocati": 0, "medaglie": []
         }
     s = st.session_state['atleti_stats'][nome_atleta]
-    s['pf'] += pf
-    s['ps'] += ps
-    s['sv'] += sv
-    s['sp'] += sp
+    s['pf'] += pf; s['ps'] += ps; s['sv'] += sv; s['sp'] += sp
     s['partite_vinte'] += vittorie
     if piazzamento == 1: s['medaglie'].append("🥇")
     elif piazzamento == 2: s['medaglie'].append("🥈")
@@ -49,11 +45,3 @@ def assegna_punti_finali(teams):
         for atleta in [team['p1'], team['p2']]:
             if atleta != "N/A":
                 st.session_state['ranking_atleti'][atleta] = st.session_state['ranking_atleti'].get(atleta, 0) + punti
-
-def registra_incasso_torneo(teams):
-    totale = sum(t.get('quota', 0) for t in teams if t.get('pagato', False))
-    if totale > 0:
-        st.session_state['storico_incassi'].append({
-            "Data": datetime.now().strftime("%d/%m/%Y"),
-            "Incasso": f"{totale} €", "Squadre": len(teams)
-        })
