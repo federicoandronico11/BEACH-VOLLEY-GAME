d474acfa8dd5b5d4186b065069838006da08ae88
federicoandronico11 <federicoandronico11@gmail.com>
2026-02-25 18:59:58 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,30 +1,22 @@
 import streamlit as st
 
 def init_session():
+    # Database Storici
     if 'db_atleti' not in st.session_state: st.session_state['db_atleti'] = []
-    if 'db_teams' not in st.session_state: st.session_state['db_teams'] = []
     if 'ranking_atleti' not in st.session_state: st.session_state['ranking_atleti'] = {}
+    if 'db_teams' not in st.session_state: st.session_state['db_teams'] = []
     if 'albo_oro' not in st.session_state: st.session_state['albo_oro'] = []
     
+    # Stato Torneo Corrente
     if 'teams' not in st.session_state: st.session_state['teams'] = []
     if 'matches' not in st.session_state: st.session_state['matches'] = []
     if 'playoffs' not in st.session_state: st.session_state['playoffs'] = []
     if 'phase' not in st.session_state: st.session_state['phase'] = "Setup"
-    if 'min_teams' not in st.session_state: st.session_state['min_teams'] = 4
 
-def assegna_punti_proporzionali(classifica_finale):
-    """
-    Assegna punti a TUTTI i partecipanti.
-    Formula: (Totale_Squadre - Posizione + 1) * 10
-    """
-    n_teams = len(classifica_finale)
-    for i, team_name in enumerate(classifica_finale):
-        posizione = i + 1
-        punti = (n_teams - posizione + 1) * 10
-        
-        # Recupera i nomi degli atleti dal dizionario team
-        team_obj = next(t for t in st.session_state['teams'] if t['full'] == team_name)
-        atleti = [team_obj['p1'], team_obj['p2']]
-        
-        for atleta in atleti:
+def assegna_punti_finali(classifica_ordinata):
+    """Assegna punti decrescenti a tutti basati sul numero di partecipanti."""
+    n = len(classifica_ordinata)
+    for i, team in enumerate(classifica_ordinata):
+        punti = (n - i) * 10  # Esempio: 1° su 10 prende 100pt, l'ultimo 10pt
+        for atleta in [team['p1'], team['p2']]:
             st.session_state['ranking_atleti'][atleta] = st.session_state['ranking_atleti'].get(atleta, 0) + punti
