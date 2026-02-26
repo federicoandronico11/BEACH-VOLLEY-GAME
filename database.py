6b333acbef58bf3d08bfb46f6be8d24ce9352c91
federicoandronico11 <federicoandronico11@gmail.com>
2026-02-26 01:18:13 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,55 +1,49 @@
 import streamlit as st
+import pandas as pd
 from datetime import datetime
 
 def init_session():
-    # Database Storici (Permanenti)
-    if 'db_atleti' not in st.session_state: st.session_state['db_atleti'] = []
-    if 'ranking_atleti' not in st.session_state: st.session_state['ranking_atleti'] = {}
-    if 'albo_oro' not in st.session_state: st.session_state['albo_oro'] = []
-    if 'storico_incassi' not in st.session_state: st.session_state['storico_incassi'] = []
-    if 'atleti_stats' not in st.session_state: st.session_state['atleti_stats'] = {}
-    
-    # Stato Torneo Corrente
-    if 'teams' not in st.session_state: st.session_state['teams'] = []
-    if 'matches' not in st.session_state: st.session_state['matches'] = []
-    if 'playoffs' not in st.session_state: st.session_state['playoffs'] = []
-    if 'phase' not in st.session_state: st.session_state['phase'] = "Setup"
-    if 'match_type' not in st.session_state: st.session_state['match_type'] = "Best of 3"
+    # Inizializzazione di tutte le variabili di stato se non esistono
+    keys = {
+        'db_atleti': [], 'ranking_atleti': {}, 'atleti_stats': {}, 
+        'storico_incassi': [], 'teams': [], 'matches': [], 'playoffs': [],
+        'phase': "Setup", 'match_type': "Best of 3",
+        'settings': {"punti_set": 21, "punti_tiebreak": 15, "formato": "Gironi + Playoff"}
+    }
+    for key, val in keys.items():
+        if key not in st.session_state:
+            st.session_state[key] = val
 
-def aggiorna_database_storico(nome_atleta, pf, ps, sv, sp, vittorie, piazzamento):
-    if nome_atleta == "N/A": return # Esclude i BYE
-    if nome_atleta not in st.session_state['atleti_stats']:
-        st.session_state['atleti_stats'][nome_atleta] = {
-            "pf": 0, "ps": 0, "sv": 0, "sp": 0, 
-            "partite_vinte": 0, "tornei_giocati": 0, "medaglie": []
-        }
-    s = st.session_state['atleti_stats'][nome_atleta]
-    s['pf'] += pf
-    s['ps'] += ps
-    s['sv'] += sv
-    s['sp'] += sp
-    s['partite_vinte'] += vittorie
-    
-    if piazzamento == 1: s['medaglie'].append("🥇")
-    elif piazzamento == 2: s['medaglie'].append("🥈")
-    elif piazzamento == 3: s['medaglie'].append("🥉")
+def registra_risultato_completo(match):
+    """Aggiorna le carriere degli atleti basandosi sui risultati del match"""
+    for team_key, prefix in [('A', 'S'), ('B', 'S')]:
+        team = match[team_key]
+        opp_key = 'B' if team_key == 'A' else 'A'
+        
+        # Calcolo punti fatti/subiti e set vinti/persi
+        pf = match['S1'+team_key] + match.get('S2'+team_key, 0) + match.get('S3'+team_key, 0)
+        ps = match['S1'+opp_key] + match.get('S2'+opp_key, 0) + match.get('S3'+opp_key, 0)
+        
+        # Calcolo set vinti
+        sv = (1 if match['S1'+team_key] > match['S1'+opp_key] else 0)
+        if st.session_state.match_type == "Best of 3":
+            sv += (1 if match['S2'+team_key] > match['S2'+opp_key] else 0)
+            sv += (1 if match.get('S3'+team_key, 0) > match.get('S3'+opp_key, 0) else 0)
+        
+        sp = 1 if st.session_state.match_type == "Set Unico" else (2 if sv < 2 else 1 if (match.get('S3A',0) > 0) else 0)
+        win = sv >= (1 if st.session_state.match_type == "Set Unico" else 2)
 
-def chiudi_torneo_atleta(nome_atleta):
-    if nome_atleta in st.session_state['atleti_stats']:
-        st.session_state['atleti_stats'][nome_atleta]['tornei_giocati'] += 1
-
-def registra_incasso_torneo(teams):
-    totale = sum(t.get('quota', 0) for t in teams if t.get('pagato', False))
-    if totale > 0:
-        st.session_state['storico_incassi'].append({
-            "Data": datetime.now().strftime("%d/%m/%Y"),
-            "Incasso": f"{totale} €", "Squadre": len(teams)
-        })
-
-def assegna_punti_finali(teams):
-    n = len(teams)
-    for i, team in enumerate(teams):
-        punti = (n - i) * 10
         for atleta in [team['p1'], team['p2']]:
-            if atleta != "N/A":
-                st.session_state['ranking_atleti'][atleta] = st.session_state['ranking_atleti'].get(atleta, 0) + punti
+            if atleta in ["", "N/A", "BYE"]: continue
+            if atleta not in st.session_state.atleti_stats:
+                st.session_state.atleti_stats[atleta] = {"pf":0,"ps":0,"sv":0,"sp":0,"v":0,"p":0,"history":[]}
+            
+            s = st.session_state.atleti_stats[atleta]
+            s['pf'] += pf; s['ps'] += ps; s['sv'] += sv; s['sp'] += sp
+            if win: s['v'] += 1
+            else: s['p'] += 1
+            s['history'].append(pf - ps)
+
+def esporta_storico():
+    if not st.session_state.storico_incassi: return None
+    return pd.DataFrame(st.session_state.storico_incassi).to_csv(index=False)
